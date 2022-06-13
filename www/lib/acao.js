b6a3ff90368daf3ceb572a70e769c4553ea74c69
PedroLeGarcia <bstoolk@gmail.com>
2022-06-13 14:07:36 +0900

    eu to louco

--- www/lib/acao.js
+++ www/lib/acao.js
@@ -1,18 +1,125 @@
 window.onload = function(){
+    var url = 'https://profrodolfo.com.br/projeto/';
+    var d = document.querySelector('.dados');
 
-var url = "https://profrodolfo.com.br/projeto/";
-fetch(url)
-.then(resposta => {
-	return resposta.json();
-})
-.then( function (json){
-	var input = document.querySelector('#pesquisar');
-    var texto = '';
-    for(i= 0; i < json.length ; i++){
-        texto += '<p>'+json[i].nome+'</p>';
-        texto += '<p>'+json[i].valor+'</p>';
+    function ExibirProdutos(){
+        fetch(url)
+        .then(resposta => {
+            return resposta.json();
+        })
+        .then((json)=>{        
+            for(i = 0; i < json.length; i++){
+            		d.innerHTML+= `
+                    <div class="container">
+                        <center>
+                        <div class="row-2">
+                            <img class="img-fluid" src="https://profrodolfo.com.br/projeto/${json[i].foto}" style="border-radius:5px">
+                        </div>
+                        <div class="col-7">
+                            <h1 style="font-family: 'Mulish', sans-serif; color:white">${json[i].nome}</h1>
+                            <h3 style="font-family: 'Mulish', sans-serif; color:white">R$: ${json[i].valor}</h3>
+                            <hr>
+                        </div>
+                        </center>
+                    </div>
+                	`;
+            }
+        }).catch();
     }
-     input.innerHTML += texto;
-})
-.catch();
+    ExibirProdutos();
+ function ExibirPorNome(nome){
+    	fetch(url)
+        .then(resposta => {
+            return resposta.json();
+        })
+        .then((json)=>{
+        	nome = nome.toUpperCase();      
+            for(i = 0; i < json.length; i++){
+            	if(json[i].nome.toUpperCase().includes(nome)){
+            		d.innerHTML+= `
+                    <div class="container">
+                        <center>
+                        <div class="row-2">
+                            <img class="img-fluid" src="https://profrodolfo.com.br/projeto/${json[i].foto}" style="border-radius:5px">
+                        </div>
+                        <div class="col-7">
+                            <h1 style="font-family: 'Mulish', sans-serif; color:white">${json[i].nome}</h1>
+                            <h3 style="font-family: 'Mulish', sans-serif; color:white">R$: ${json[i].valor}</h3>
+                            <hr>
+                        </div>
+                        </center>
+                    </div>
+                	`;
+            	}
+            }
+        }).catch();
+    }
+
+    function ExibirPorPreco(preco){
+    	fetch(url)
+        .then(resposta => {
+            return resposta.json();
+        })
+        .then((json)=>{        
+            for(i = 0; i < json.length; i++){
+            	if(json[i].valor<=preco){
+            		d.innerHTML+= `
+                    <div class="container">
+                        <center>
+                        <div class="row-2">
+                            <img class="img-fluid" src="https://profrodolfo.com.br/projeto/${json[i].foto}" style="border-radius:5px">
+                        </div>
+                        <div class="col-7">
+                            <h1 style="font-family: 'Mulish', sans-serif; color:white">${json[i].nome}</h1>
+                            <h3 style="font-family: 'Mulish', sans-serif; color:white">R$: ${json[i].valor}</h3>
+                            <hr>
+                        </div>
+                        </center>
+                    </div>
+                	`;
+            	}
+            }
+        }).catch();
+    }
+
+    function ExibirPorPrecoENome(nome, preco){
+    	fetch(url)
+        .then(resposta => {
+            return resposta.json();
+        })
+        .then((json)=>{        
+        	nome = nome.toUpperCase();
+            for(i = 0; i < json.length; i++){
+            	if(json[i].valor<=preco && json[i].nome.toUpperCase().includes(nome)){
+            		d.innerHTML+= `
+                    <div class="container">
+                        <center>
+                        <div class="row-2">
+                            <img class="img-fluid" src="https://profrodolfo.com.br/projeto/${json[i].foto}" style="border-radius:5px">
+                        </div>
+                        <div class="col-7">
+                            <h1 style="font-family: 'Mulish', sans-serif; color:white">${json[i].nome}</h1>
+                            <h3 style="font-family: 'Mulish', sans-serif; color:white">R$: ${json[i].valor}</h3>
+                            <hr>
+                        </div>
+                        </center>
+                    </div>
+                	`;
+            	}
+            }
+        }).catch();
+    }
+    
+    document.querySelector('#button').addEventListener('click', ()=>{
+    	d.innerHTML = '';
+    	let nome = document.querySelector('#pesquisarNome').value;
+    	let preco = Number(document.querySelector('#pesquisarPreco').value);
+    	if(!nome){
+    		ExibirPorPreco(preco);
+    	}else if(!preco){
+    		ExibirPorNome(nome);
+    	}else if(nome && preco){
+    		ExibirPorPrecoENome(nome, preco);
+    	}
+    })
 }
